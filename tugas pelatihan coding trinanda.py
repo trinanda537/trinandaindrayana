3e3030086b09ce723239430292bd70b63918665b
trinanda537 <143860349+trinanda537@users.noreply.github.com>
2023-09-08 14:44:04 +0700

    Add files via upload

---
+++ tugas pelatihan coding trinanda.py
@@ -0,0 +1,20 @@
+#hitung volume balok
+
+p=6
+l=6
+t=9
+
+v=p*l*t
+print("print hasil hitung volume balok=",v)
+
+
+#hitung volume tabung
+
+
+pi=3.14
+r=9
+t=5
+
+v=pi*r**2*t
+
+print("hitung volume tabung =",v)
